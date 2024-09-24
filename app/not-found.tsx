13de8e97acf7c4db254653a9168b2d3a2ddbe902
User0k <glucus2@yandex.ru>
2024-09-24 14:30:38 +0300

    feat: not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,22 @@
+import Link from 'next/link';
+import { inter } from './fonts';
+import BtnBack from './ui/BtnBack';
+import { GITHUB_URL } from './constants/paths';
+import './not-found.css';
+
 export default function NotFound() {
   return (
     <main id="not-found">
-      <h1>Not Found</h1>
+      <article className={inter.className}>
+        <h1>Not Found</h1>
+        <p>Oh no! This page doesn`t exist!</p>
+        <p className="not-found-actions">
+          <BtnBack /> |{' '}
+          <Link href={`${GITHUB_URL}/issues`} className="link-issue">
+            Create an issue
+          </Link>
+        </p>
+      </article>
     </main>
   );
 }
