da6c4862aedd13393475650b9a9b205261e7fdc2
User0k <glucus2@yandex.ru>
2024-10-03 13:40:09 +0300

    feat: add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
 import type { Metadata } from 'next';
+import { GoogleAnalytics } from '@next/third-parties/google';
 import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Exploring Frontend Development: Tips and Tricks',
   description:
     'Embark on a journey through the ever-evolving landscape of JavaScript, TypeScript, and a myriad of frontend technologies including React, Next.js, Redux, and advanced state management. Explore expert insights, cutting-edge techniques, and in-depth articles that elevate your development prowess and illuminate the path to frontend mastery.',
+  keywords:
+    'frontend, web, web-development, software, javascript, typescript, react',
+  openGraph: {
+    title: 'Exploring Frontend Development: Tips and Tricks',
+    description: `Discover the evolving world of JavaScript, TypeScript, React, Next.js, Redux, and more. Gain expert insights, advanced techniques, and articles to enhance your frontend development skills.`,
+  },
 };
 
 export default function RootLayout({
@@ -15,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <GoogleAnalytics gaId="G-PNZXPNESPL" />
     </html>
   );
 }
