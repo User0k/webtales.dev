b13c6a26810da9f7245b319ca472a46dea7488c5
User0k <glucus2@yandex.ru>
2024-10-08 23:46:55 +0300

    fix: ribbon colors

--- app/utils/generateRibbonColors.ts
+++ app/utils/generateRibbonColors.ts
@@ -1,6 +1,7 @@
 import { DARK_COLORS, LIGHT_COLORS } from '../constants/colors';
 
 export default function generateRibbonColors(tag: string) {
+  tag = tag.toLowerCase();
   const backgroundColor =
     DARK_COLORS.includes(tag) || LIGHT_COLORS.includes(tag)
       ? `var(--color-${tag})`
